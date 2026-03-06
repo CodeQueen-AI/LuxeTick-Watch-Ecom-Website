9f74a507787627319fe21eed88a7d49e193eb59c
CodeQueen-AI <codeq209@gmail.com>
2026-03-06 21:13:14 +0500

    update

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -1,11 +1,56 @@
+// import Image from "next/image";
+
+// export default function HomePage() {
+//   return (
+//     <section className="w-full min-h-screen relative flex">
+
+//       {/* Left Side */}
+//       <div className="w-1/2 bg-[#09162c] flex items-center justify-center">
+//         <Image
+//           src="/watch1.png"
+//           alt="left image"
+//           width={220}
+//           height={220}
+//           className="object-contain"
+//         />
+//       </div>
+
+//       {/* Right Side */}
+//       <div className="w-1/2 bg-white flex items-center justify-center">
+//         <Image
+//           src="/watch2.png"
+//           alt="right image"
+//           width={220}
+//           height={220}
+//           className="object-contain"
+//         />
+//       </div>
+
+//       {/* Center Image */}
+//       <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
+//         <Image
+//           src="/watch3.png"
+//           alt="center image"
+//           width={220}
+//           height={220}
+//           className="object-contain"
+//         />
+//       </div>
+
+//     </section>
+//   );
+// }
+
+
+
 import Image from "next/image";
 
 export default function HomePage() {
   return (
-    <section className="w-full min-h-screen relative flex">
+    <section className="w-full h-[85vh] relative flex">
 
       {/* Left Side */}
-      <div className="w-1/2 bg-[#0b1d3a] flex items-center justify-center">
+      <div className="w-1/2 bg-[#09162c] flex items-start justify-center pt-8">
         <Image
           src="/watch1.png"
           alt="left image"
@@ -16,7 +61,7 @@ export default function HomePage() {
       </div>
 
       {/* Right Side */}
-      <div className="w-1/2 bg-white flex items-center justify-center">
+      <div className="w-1/2 bg-white flex items-start justify-center pt-8">
         <Image
           src="/watch2.png"
           alt="right image"
@@ -27,7 +72,7 @@ export default function HomePage() {
       </div>
 
       {/* Center Image */}
-      <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
+      <div className="absolute left-1/2 top-[40%] -translate-x-1/2 -translate-y-1/2">
         <Image
           src="/watch3.png"
           alt="center image"
