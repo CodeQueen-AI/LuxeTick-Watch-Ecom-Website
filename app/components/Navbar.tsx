20c7caf0c8525ed848bfe0c4f638479db19c8374
CodeQueen-AI <codeq209@gmail.com>
2026-03-06 21:01:16 +0500

    update

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -4,10 +4,30 @@ import { BsCart3 } from "react-icons/bs";
 export default function Page() {
   return (
     <>
-      {/* Google Font */}
+      {/* Google Fonts */}
       <style>
         {`
         @import url('https://fonts.googleapis.com/css2?family=Allura&display=swap');
+        @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600&display=swap');
+
+        .nav-link{
+          position: relative;
+        }
+
+        .nav-link::after{
+          content: "";
+          position: absolute;
+          left: 0;
+          bottom: -4px;
+          width: 0%;
+          height: 2px;
+          background: black;
+          transition: width 0.35s ease;
+        }
+
+        .nav-link:hover::after{
+          width: 100%;
+        }
         `}
       </style>
 
@@ -21,17 +41,21 @@ export default function Page() {
 
         {/* Logo */}
         <h1
-          className="text-6xl text-white tracking-tight"
+          className="text-5xl text-white tracking-tight"
           style={{ fontFamily: "'Allura', cursive" }}
         >
           Omega
         </h1>
 
         {/* Menu */}
-        <div className="flex items-center gap-8 text-black font-medium">
-          <a href="#" className="hover:underline">Home</a>
-          <a href="#" className="hover:underline">Watch</a>
-          <a href="#" className="hover:underline">Contact</a>
+        <div
+          className="flex items-center gap-8 text-black font-medium"
+          style={{ fontFamily: "'Poppins', sans-serif" }}
+        >
+          <a href="#" className="nav-link">HOME</a>
+          <a href="#" className="nav-link">COLLECTIONS</a>
+          <a href="#" className="nav-link">SERVICES</a>
+          <a href="#" className="nav-link mr-10">CONTACT</a>
 
           <BsCart3 size={24} className="cursor-pointer" />
         </div>
