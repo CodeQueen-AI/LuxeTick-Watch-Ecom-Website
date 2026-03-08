2549815719da7049b6cb16417630c6f2b84dc0f2
CodeQueen-AI <codeq209@gmail.com>
2026-03-08 16:49:33 +0500

    update

--- app/components/Section14.tsx
+++ app/components/Section14.tsx
@@ -1,53 +1,34 @@
 "use client";
 
-import { Poppins } from "next/font/google";
-
-const poppins = Poppins({
-  subsets: ["latin"],
-  weight: ["400","500","600","700"],
-});
-
 export default function Collection() {
   return (
-    <section className={`${poppins.className} py-20`}>
-
+    <section className="poppins py-20">
       <h1 className="text-center text-4xl font-semibold mb-12">
         Collection
       </h1>
-
       <div className="w-[70%] mx-auto grid grid-cols-3 gap-5">
-
-        {/* Column 1 */}
         <div className="relative h-[450px] bg-[url('/rado.jpg')] bg-cover bg-center group">
           <Overlay name="Rado" />
         </div>
-
-        {/* Column 2 */}
         <div className="flex flex-col gap-5">
           <div className="relative h-[215px] bg-[url('/Tissot.jpg')] bg-cover bg-center group">
             <Overlay name="Tissot" />
           </div>
-
           <div className="relative h-[215px] bg-[url('/cartier.jpg')] bg-cover bg-center group">
             <Overlay name="Cartier" />
           </div>
         </div>
-
-        {/* Column 3 */}
         <div className="flex flex-col gap-5">
           <div className="relative h-[140px] bg-[url('/geneve.jpg')] bg-cover bg-center group">
             <Overlay name="Geneve" />
           </div>
-
           <div className="relative h-[140px] bg-[url('/omega.jpg')] bg-cover bg-center group">
             <Overlay name="Omega" />
           </div>
-
           <div className="relative h-[140px] bg-[url('/Fitron.jpg')] bg-cover bg-center group">
             <Overlay name="Fitron" />
           </div>
         </div>
-
       </div>
     </section>
   );
@@ -57,7 +38,6 @@ function Overlay({ name }: { name: string }) {
   return (
     <>
       <div className="absolute inset-0 bg-black/50 scale-x-0 origin-right transition-transform duration-700 group-hover:scale-x-100 group-hover:origin-left"></div>
-
       <div className="absolute inset-0 flex items-center justify-center opacity-0 group-hover:opacity-100 transition duration-700">
         <span className="bg-[#e8cfa8] px-6 py-3 uppercase text-sm text-white">
           {name}
