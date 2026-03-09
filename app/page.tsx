6137e484fabfc0c7867f342f4536d4e7ea126a7c
CodeQueen-AI <codeq209@gmail.com>
2026-03-10 00:25:35 +0500

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,18 @@
 import Navbar from './components/Navbar'
 import Home from './components/Hero'
-import Features from './components/collections'
+import Sec1 from './components/Sec1'
+import Sec2 from './components/Sec2'
+import Sec3 from './components/Sec3'
+import Sec4 from './components/Sec4'
+import Sec5 from './components/Sec5'
+import Sec6 from './components/Sec6'
+import Sec7 from './components/Sec7'
 import Section1 from './components/Section1'
 import Section2 from './components/Section2'
-import Section3 from './components/Section3'
-import Section4 from './components/Section4'
-import Section5 from './components/Section5'
-import Section6 from './components/Section6'
 import Section7 from './components/Section7'
 import Section8 from './components/Section8'
 import Section9 from './components/Section9'
-import Section10 from './components/Section10'
 import Section11 from './components/Section11'
-import Section12 from './components/Section12'
 import Section13 from './components/Section13'
 import Section14 from './components/Section14'
 import Footer from './components/footer'
@@ -21,19 +21,19 @@ export default function Page() {
     <div>
     <Navbar/>
     <Home/>
-    <Features/>
+    <Sec1/>
+    <Sec2/>
+    <Sec3/>
+    <Sec4/>
+    <Sec5/>
+    <Sec6/>
+    <Sec7/>
     <Section1/>
     <Section2/>
-    <Section3/>
-    <Section4/>
-    <Section5/>
-    <Section6/>
     <Section7/>
     <Section8/>
     <Section9/>
-    <Section10/>
     <Section11/>
-    <Section12/>
     <Section13/>
     <Section14/>
     <Footer/>
