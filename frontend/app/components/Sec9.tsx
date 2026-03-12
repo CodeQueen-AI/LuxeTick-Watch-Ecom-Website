af30b1bb2a699c64341ec09c335e735191f7db5d
CodeQueen-AI <codeq209@gmail.com>
2026-03-12 21:47:37 +0500

    update

--- frontend/app/components/Sec9.tsx
+++ frontend/app/components/Sec9.tsx
@@ -24,7 +24,7 @@ export default function RacingSection() {
         </div>
         <div className="relative h-[500px] lg:h-auto">
           <Image
-            src="/Imges/Img6.webp"
+            src="/Imges/Img7.webp"
             alt="watch"
             fill
             className="object-cover"/>
