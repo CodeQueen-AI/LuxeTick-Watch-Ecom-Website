bce55ace336164ffca72e802e07187158b08d2e9
CodeQueen-AI <codeq209@gmail.com>
2026-03-14 14:30:55 +0500

    update

---
+++ backend/Models/user.js
@@ -0,0 +1,13 @@
+import mongoose from 'mongoose';
+
+mongoose.connect('mongodb://127.0.0.1:27017/watchAuthDB')
+
+const userSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+}, { timestamps: true });
+
+const User = mongoose.model('User', userSchema);
+
+export default User;
