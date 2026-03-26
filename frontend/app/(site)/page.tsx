6c8a715c1e8a90c74ab3160ddab75f3238e2281d
CodeQueen-AI <codeq209@gmail.com>
2026-03-27 00:07:03 +0500

    'update

---
+++ frontend/app/(site)/page.tsx
@@ -0,0 +1,34 @@
+import Home from "../components/Hero"
+import Sec1 from '../components/Sec1'
+import Sec2 from '../components/Sec2'
+import Sec3 from '../components/Sec3'
+import Sec4 from '../components/Sec4'
+import Sec5 from '../components/Sec5'
+import Sec6 from '../components/Sec6'
+import Sec7 from '../components/Sec7'
+import Sec8 from '../components/Sec8'
+import Sec9 from '../components/Sec9'
+import Sec10 from '../components/Sec10'
+import Sec11 from '../components/Sec11'
+import Sec12 from '../components/Sec12'
+import Sec13 from '../components/Sec13'
+export default function Page() {
+  return (
+    <div>
+    <Home/>
+    <Sec1/>
+    <Sec2/>
+    <Sec3/>
+    <Sec4/>
+    <Sec5/>
+    <Sec6/>
+    <Sec7/>
+    <Sec8/>
+    <Sec9/>
+    <Sec10/>
+    <Sec11/>
+    <Sec12/>
+    <Sec13/>
+    </div>
+  );
+}
