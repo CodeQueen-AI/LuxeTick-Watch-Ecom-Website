3478fa6e2056fde1e20270376a0a3be2889a8969
CodeQueen-AI <codeq209@gmail.com>
2026-03-27 17:01:58 +0500

    update

--- backend/products/routes/products.js
+++ backend/products/routes/products.js
@@ -1,9 +1,21 @@
+// import express from "express";
+// import { createProduct, getAllProducts, upload } from "../controllers/productController.js";
+
+// const router = express.Router();
+
+// router.post("/add", upload.single('image'), createProduct);   // Image upload ke liye
+// router.get("/all", getAllProducts);
+
+// export default router;
+
+
+
 import express from "express";
 import { createProduct, getAllProducts, upload } from "../controllers/productController.js";
 
 const router = express.Router();
 
-router.post("/add", upload.single('image'), createProduct);   // Image upload ke liye
-router.get("/all", getAllProducts);
+router.post("/add", upload.single('image'), createProduct);
+router.get("/", getAllProducts); // ✅ FIX
 
 export default router;
