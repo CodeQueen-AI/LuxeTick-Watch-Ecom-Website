f7756882c42207f3b89640b539fd8833e02e3c17
CodeQueen-AI <codeq209@gmail.com>
2026-03-27 19:12:38 +0500

    'update

--- frontend/app/components/clientwrapper.tsx
+++ frontend/app/components/clientwrapper.tsx
@@ -1,95 +1,4 @@
-// "use client";
-// import { ReactNode } from "react";
-
-// export default function ClientWrapper({ children }: { children: ReactNode }) {
-//   return <div>{children}</div>;
-// }
-
-
-
-
-
-
-
-
-
-// // components/ClientWrapper.tsx
-// "use client";
-
-// import { Poppins, Allura } from "next/font/google";
-
-// const poppins = Poppins({
-//   subsets: ["latin"],
-//   weight: ["300","400","500","600","700"],
-//   variable: "--font-poppins",
-// });
-
-// const allura = Allura({
-//   subsets: ["latin"],
-//   weight: "400",
-//   variable: "--font-allura",
-// });
-
-// export default function ClientWrapper({ children }: { children: React.ReactNode }) {
-//   // Fonts ab client-side pe load honge, SSR mismatch avoid hoga
-//   return (
-//     <div className={`${poppins.variable} ${allura.variable}`}>
-//       {children}
-//     </div>
-//   );
-// }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// // components/ClientWrapper.tsx
-// "use client";
-
-// import { Poppins, Allura } from "next/font/google";
-// import { ReactNode } from "react";
-
-// const poppins = Poppins({
-//   subsets: ["latin"],
-//   weight: ["300","400","500","600","700"],
-//   variable: "--font-poppins",
-// });
-
-// const allura = Allura({
-//   subsets: ["latin"],
-//   weight: "400",
-//   variable: "--font-allura",
-// });
-
-// export default function ClientWrapper({ children }: { children: ReactNode }) {
-//   return (
-//     <div className={`${poppins.variable} ${allura.variable}`}>
-//       {children}
-//     </div>
-//   );
-// }
-
-
-
-
-
-
-
 "use client";
-
 import { Poppins, Allura } from "next/font/google";
 import { ReactNode } from "react";
 
@@ -106,7 +15,6 @@ const allura = Allura({
 });
 
 type Props = { children: ReactNode };
-
 export default function ClientWrapper({ children }: Props) {
   return (
     <div className={`${poppins.variable} ${allura.variable}`}>
