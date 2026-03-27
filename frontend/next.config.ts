e84c24c648424e557fde0a494a80d3629fc28d8e
CodeQueen-AI <codeq209@gmail.com>
2026-03-27 19:28:42 +0500

    update

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,31 +1,8 @@
-// import type { NextConfig } from "next";
-
-// const nextConfig: NextConfig = {
-//   /* config options here */
-// };
-
-// export default nextConfig;
-
-
-
-
-// import type { NextConfig } from "next";
-
-// const nextConfig: NextConfig = {
-//   images: {
-//     domains: ["localhost"],
-//   },
-// };
-
-// export default nextConfig;
-
-
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ["localhost", "res.cloudinary.com"], // added Cloudinary
+    domains: ["localhost", "res.cloudinary.com"], 
   },
 };
-
 export default nextConfig;
