a0ad57687babaeb3a625cfe70327f0027f1867aa
CodeQueen-AI <codeq209@gmail.com>
2026-04-01 00:48:13 +0500

    update'

--- frontend/app/(site)/products/[id]/page.tsx
+++ frontend/app/(site)/products/[id]/page.tsx
@@ -4,8 +4,6 @@ import { useState } from "react";
 import Image from "next/image";
 import { FaRegHeart, FaHeart } from "react-icons/fa";
 import { AiOutlineCheckCircle } from "react-icons/ai";
-// import { useCart } from "../../context/cartcontext";
-// import { useWishlist } from "../../context/wishlistcontext";
 
 import {useCart} from "@/app/(site)/context/cartcontext"
 import { useWishlist } from "@/app/(site)/context/wishlistcontext";
