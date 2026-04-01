e1932f8789643d9700134d8cd7be945ca446c850
CodeQueen-AI <codeq209@gmail.com>
2026-04-01 22:13:38 +0500

    'update

--- frontend/app/(dashboard)/admin-login/page.tsx
+++ frontend/app/(dashboard)/admin-login/page.tsx
@@ -1,5 +1,4 @@
 "use client";
-
 import { useState } from "react";
 import { useRouter } from "next/navigation";
 import { ADMIN_EMAIL, ADMIN_PASSWORD } from "@/app/(dashboard)/lib/auth";
@@ -28,41 +27,36 @@ export default function AdminLogin() {
       
       <form
         onSubmit={handleLogin}
-        className="bg-white/80 backdrop-blur-md p-10 rounded-3xl shadow-xl w-[380px] border border-gray-200"
+        className="bg-white/80 backdrop-blur-md p-10 shadow-xl w-[380px] border border-gray-200"
       >
         {/* Title */}
-        <h2 className="text-3xl font-bold text-center text-gray-800 mb-8">
+        <h2 className="text-3xl font-extralight font-serif  text-center mb-8">
           Admin Login
         </h2>
-        <p className="text-center text-gray-500 text-sm mb-8">
-          Dashboard Access
-        </p>
 
         {/* Email */}
         <div className="mb-6">
-          <label className="text-gray-600 text-sm">Email</label>
+          <label className="text-lg">Email</label>
           <input
             type="email"
-            placeholder="Enter your email"
-            className="w-full mt-2 px-2 py-2 bg-transparent border-b-2 border-gray-300 focus:border-indigo-500 outline-none transition"
+            className="w-full mt-2 px-2 py-2 bg-transparent border-b-2 border-gray-300 focus:border-[#09162c] outline-none transition"
             onChange={(e) => setEmail(e.target.value)}
           />
         </div>
 
         {/* Password */}
         <div className="mb-8 relative">
-          <label className="text-gray-600 text-sm">Password</label>
+          <label className="text-sm">Password</label>
           <input
             type={show ? "text" : "password"}
-            placeholder="Enter your password"
-            className="w-full mt-2 px-2 py-2 bg-transparent border-b-2 border-gray-300 focus:border-indigo-500 outline-none transition pr-10"
+            className="w-full mt-2 px-2 py-2 bg-transparent border-b-2 border-gray-300 focus:border-[#09162c] outline-none transition pr-10"
             onChange={(e) => setPassword(e.target.value)}
           />
 
           {/* Eye Icon */}
           <span
             onClick={() => setShow(!show)}
-            className="absolute right-2 top-9 cursor-pointer text-gray-500 hover:text-indigo-600"
+            className="absolute right-2 top-9 cursor-pointer text-gray-500 hover:text-[#09162c]"
           >
             {show ? <FiEyeOff size={20} /> : <FiEye size={20} />}
           </span>
@@ -71,7 +65,7 @@ export default function AdminLogin() {
         {/* Button */}
         <button
           type="submit"
-          className="w-full py-3 rounded-xl bg-indigo-600 text-white font-semibold hover:bg-indigo-700 transition-all duration-300 shadow-md hover:shadow-lg"
+          className="w-full py-3 bg-[#09162c] text-white font-semibold hover:bg-[#09162c] transition-all duration-300 shadow-md hover:shadow-lg cursor-pointer"
         >
           Login to Dashboard
         </button>
